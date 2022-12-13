9a47f9d4c35907f812dbcd2339878c963f490399
prashanta <pbt9840@gmail.com>
2022-12-13 15:18:42 +0545

    node version managed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <div>
+    <div >
       lama dev
     </div>
   );
--- src/pages/home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import "./home.scss";
-function Home() {
-  return (
-  <div className="home">Home</div>
-  )
-}
-
-export default Home;
