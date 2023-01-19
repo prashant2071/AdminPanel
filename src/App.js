e7ec8e1307ae1c66dd0f0e25ce9007658e3d1161
prashanta <pbt9840@gmail.com>
2023-01-19 23:56:54 +0545

    react router added!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Login from './pages/login/Login'
+import Single from './pages/single/Single'
+import New from './pages/new/New';
 
+
+import {
+  Routes,
+  Route,
+  BrowserRouter
+} from "react-router-dom";
 function App() {
   return (
     <div >
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='users'>
+              <Route index element={<List/>}/>
+              <Route path=':userId' element={<Single/>}/>
+              <Route path='new' element={<New/>}/>
+          </Route>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
