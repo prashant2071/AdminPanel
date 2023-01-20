1b525243d3f6d587ff9565c0b0518c63e0086d97
prashanta <pbt9840@gmail.com>
2023-01-20 18:29:42 +0545

    side bar complete

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,16 @@
 import "./sidebar.scss";
-
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import AddCardIcon from '@mui/icons-material/AddCard';
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -9,21 +20,70 @@ const Sidebar = () => {
       <hr/>
       <div className="center">
         <ul>
+          <p className="title">Main</p>
           <li>
+            <DashboardIcon className="icon"/>
             <span>Dashboard</span>
           </li>
+          <p className="title">List</p>
+
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon className="icon"/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreIcon className="icon"/>
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <AddCardIcon className="icon"/>
+            <span>Order</span>
+          </li>          
+          <li>
+            <LocalShippingIcon className="icon"/>
+            <span>Delevery</span>
+          </li>   
+       
+          <li>
+            <AssessmentIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <p className="title">Useful Links</p>
+          <li>
+            <NotificationsActiveIcon className="icon"/>
+            <span>Notification</span>
+          </li>
+          <li>
+            <MonitorHeartIcon className="icon"/>
+            <span>System Health</span>
+          </li>
+          <p className="title">Service</p>
+
+          <li>
+            <ErrorOutlineIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon"/>
+            <span>Setting</span>
+          </li>
+          <p className="title">User</p>
+          <li>
+            <AccountCircleRoundedIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppRoundedIcon className="icon"/>
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="buttom">color option</div>
+      <div className="buttom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+
+      </div>
     </div>
   );
 };
