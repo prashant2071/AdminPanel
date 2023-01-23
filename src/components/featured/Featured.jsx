dd32b51b12a8460daac0732f6a0b6738af1694e0
prashanta <pbt9840@gmail.com>
2023-01-23 22:35:37 +0545

    circular charts added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,18 @@
 import './featured.scss'
-
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 const Featured = () => {
   return (
-    <div className='feature'>Featured</div>
+    <div className='feature'>
+        <div className="top">
+            <h1 className="title">Total Revenue</h1>
+            <MoreVertIcon fontSize='small' />
+        </div>
+        <div className="bottom">
+            <div className="featuredChart">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
