ee1bb8bedce4fea95feadc4396ab37c2df4d9b5e
prashanta <pbt9840@gmail.com>
2023-01-23 23:24:29 +0545

    featured charts finish recharts added'

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,9 @@
 import './featured.scss'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {CircularProgressbar}from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css'
+import KeyboardArrowUp from '@mui/icons-material/KeyboardArrowUp';
+import { KeyboardArrowDown } from '@mui/icons-material';
 const Featured = () => {
   return (
     <div className='feature'>
@@ -9,7 +13,35 @@ const Featured = () => {
         </div>
         <div className="bottom">
             <div className="featuredChart">
-                
+            <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+            </div>
+            <p className="title">Total sales made today</p>
+            <p className='amount'>$420</p>
+            <p className="description">
+                previous transaction processing. Last payment maynot be included
+            </p>
+            <div className="summary">
+                <div className="item">
+                    <div className="itemTitle">Target</div>
+                    <div className="itemResult negitive">
+                        <KeyboardArrowDown fontSize='small'/>
+                        <div className="resultAmount">$12.4K</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Las week</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUp fontSize='small'/>
+                        <div className="resultAmount">$12.4K</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last month</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUp fontSize='small'/>
+                        <div className="resultAmount">$12.4K</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
