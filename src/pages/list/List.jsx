3368b513f058104d2dc2dc8c469278a9bae512ba
prashanta <pbt9840@gmail.com>
2023-01-25 22:39:24 +0545

    list added

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,15 @@
 import './list.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 function List() {
   return (
-    <div>List</div>
+    <div className='list'>
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+      </div>
+    </div>
   )
 }
 
