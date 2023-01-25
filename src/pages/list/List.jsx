d33b889a27334cbc8c6db1823d2998704272df61
prashanta <pbt9840@gmail.com>
2023-01-25 22:59:30 +0545

    list scss added

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,16 +1,16 @@
 import './list.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
-import Navbar from '../../components/navbar/Navbar'
-
+import Navbar from '../../components/navbar/Navbar';
 function List() {
   return (
     <div className='list'>
       <Sidebar/>
       <div className="listContainer">
-        <Navbar/>
+      <Navbar/>
+      datatables
       </div>
     </div>
   )
 }
 
-export default List
+export default List;
