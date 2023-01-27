f9654c2b0ec5802c9d2bcd71325d32690da5984c
prashanta <pbt9840@gmail.com>
2023-01-27 22:34:02 +0545

    data tables added

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,13 +1,14 @@
 import './list.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar';
+import Datatables from '../../components/datatables/Datatables';
 function List() {
   return (
     <div className='list'>
       <Sidebar/>
       <div className="listContainer">
       <Navbar/>
-      datatables
+      <Datatables/>
       </div>
     </div>
   )
